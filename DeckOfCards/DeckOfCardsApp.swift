bfef779d00583d7905fbff4e03edc20eea765c80
Louis Jaume <jaume.l@etoncollege.org.uk>
2023-03-22 09:57:15 +0000

    Initial Commit

---
+++ DeckOfCards/DeckOfCardsApp.swift
@@ -0,0 +1,17 @@
+//
+//  DeckOfCardsApp.swift
+//  DeckOfCards
+//
+//  Created by Jaume, Louis (NA) on 22/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct DeckOfCardsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
